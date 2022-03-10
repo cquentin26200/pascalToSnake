5c6e66a6dddd58372cd2d48e361a4001f513a7ae
cquentin26200 <qcheron@edenschool.fr>
2022-03-10 16:42:54 +0100

    add six function

---
+++ js/app.js
@@ -0,0 +1,3 @@
+export const pascalToSnake = (entry) => {
+    return entry;
+}
--- test/test.js
+++ test/test.js
@@ -1,8 +1,31 @@
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
+import assert from "assert";
+import {pascalToSnake} from '../js/app.js';
+
+describe("Array", function () {
+  describe("#indexOf()", function () {
+    it("should return -1 when the value is not present", function () {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
+  });
+});
+
+describe("pascalToSnake", function () {
+  it("should return test_controller", function () {
+    assert.equal(pascalToSnake('TestController'), "test_controller");
+  });
+  it("should return movie_and_book", function () {
+    assert.equal(pascalToSnake("MovieAndBook"), "movie_and_book");
+  });
+  it("should return app7_test", function () {
+    assert.equal(pascalToSnake("App7_Test"), "app7_test");
+  });
+  it("should return home", function () {
+    assert.equal(pascalToSnake("Home"), "home");
+  });
+  it("should return play-movie", function () {
+    assert.equal(pascalToSnake("PlayMovie"), "play-movie");
+  });
+  it("should return 1", function () {
+    assert.equal(pascalToSnake(1), "1");
   });
 });
