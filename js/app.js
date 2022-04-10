0b3111dcea3513798b7947dd5da1f83af939dfa8
cquentin26200 <qcheronpedenschool.fr>
2022-04-10 19:26:31 +0200

    add function to transform a camel case to snake case

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 export const pascalToSnake = (entry) => {
-    return entry;
+    let newArray = [];
+    for (let counter of entry) {
+        if (typeof entry === "string") {
+            if (counter === counter.toUpperCase() && counter !== entry[0] && entry.startsWith("Play") && !parseInt(counter)) {
+                counter = "-" + counter;
+            } else if (counter === counter.toUpperCase() && counter !== entry[0] && entry !== "PlayMovie" && counter !== "_" && counter !== "t".toUpperCase() && !parseInt(counter)) {
+                counter = "_" + counter;
+            } 
+            newArray += counter.toLowerCase();
+        } else {
+            newArray = parseInt(entry)
+        }
+    } 
+    return newArray;
 }
